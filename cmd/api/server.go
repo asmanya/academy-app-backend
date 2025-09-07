6c6416ec10321685d01c7302de4674e42b240450
asmanya <107456424+asmanya@users.noreply.github.com>
2025-09-08 03:04:24 +0530

    mux & security header + cors middlewares

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
+
+	mw "academy-app-system/internal/api/middlewares"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "Hello root route")
 	w.Write([]byte("Hello root route"))
 	fmt.Println("Hello root route")
 }
@@ -16,16 +18,13 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func teachersHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		// teachers/{id}
-		// teachers/9
-		// teachers/?key=value&query=value2&sortby=email&sortorder=ASC
-		fmt.Println(r.URL.Path)
+		// fmt.Println(r.URL.Path)
 		path := strings.TrimPrefix(r.URL.Path, "/teachers/")
 		userID := strings.TrimSuffix(path, "/")
 
-		fmt.Println("id is:", userID)
+		// fmt.Println("id is:", userID)
 
-		fmt.Println("Query params:", r.URL.Query())
+		// fmt.Println("Query params:", r.URL.Query())
 		queryParams := r.URL.Query()
 		sortby := queryParams.Get("sortby")
 		key := queryParams.Get("key")
@@ -36,12 +35,8 @@ func teachersHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		fmt.Printf("Sortby: %v, Key: %v, Sortorder:%v\n", sortby, key, sortorder)
-
-
-		w.Write([]byte("Hello GET Method on Teachers Route"))
-		// fmt.Println("Hello GET Method on Teachers Route")
+		w.Write([]byte(fmt.Sprintf("Hello GET Method on Teachers Route, the userID is %s", userID)))
 	case http.MethodPost:
-		
 		w.Write([]byte("Hello POST Method on Teachers Route"))
 		fmt.Println("Hello POST Method on Teachers Route")
 	case http.MethodPut:
@@ -54,9 +49,6 @@ func teachersHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello DELETE Method on Teachers Route"))
 		fmt.Println("Hello DELETE Method on Teachers Route")
 	}
-
-	w.Write([]byte("Hello Teachers route"))
-	fmt.Println("Hello Teachers route")
 }
 
 func studentsHandler(w http.ResponseWriter, r *http.Request) {
@@ -77,9 +69,6 @@ func studentsHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello DELETE Method Students Route"))
 		fmt.Println("Hello DELETE Method Students Route")
 	}
-
-	w.Write([]byte("Hello Students route"))
-	fmt.Println("Hello Students route")
 }
 
 func execsHandler(w http.ResponseWriter, r *http.Request) {
@@ -100,28 +89,39 @@ func execsHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello DELETE Method execs Route"))
 		fmt.Println("Hello DELETE Method execs Route")
 	}
-
-	w.Write([]byte("Hello execs route"))
-	fmt.Println("Hello execs route")
 }
 
 func main() {
 
 	port := ":3000"
 
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/teachers/", teachersHandler)
-	http.HandleFunc("/students/", studentsHandler)
-	http.HandleFunc("/execs/", execsHandler)
+	cert := "cert.pem"
+	key := "key.pem"
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/teachers/", teachersHandler)
+	mux.HandleFunc("/students/", studentsHandler)
+	mux.HandleFunc("/execs/", execsHandler)
+
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	// create custom server
+	server := &http.Server{
+		Addr: port,
+		// Handler:   mux,
+		// Handler:   mw.Cors(mux),
+		Handler:   mw.SecurityHeader(mw.Cors(mux)),
+		TLSConfig: tlsConfig,
+	}
 
 	fmt.Println("Server is listening on port", port)
-	err := http.ListenAndServe(port, nil)
+	err := server.ListenAndServeTLS(cert, key)
 	if err != nil {
 		log.Fatal("Error starting the server:", err)
 	}
 
 }
-
-// CRUD Operations --> CRUD stands for Create, Read, Update, Delete
-// HTTP methods are used to perform actions on resources in database --> POST, GET, PUT, DELETE, PATCH
-// these methods corresponds to the CRUD operations
